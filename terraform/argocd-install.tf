4d7f96985ab11401874adfcdeccc4c4199b18830
Tobias Berg <tobiaswillyberg@gmail.com>
2025-07-02 20:59:17 +0200

    test

--- terraform/argocd-install.tf
+++ terraform/argocd-install.tf
@@ -1,17 +1,10 @@
-terraform {
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.1"
-    }
-  }
-}
-
-provider "null" {}
-
+ 
 resource "null_resource" "install_argocd_and_app_of_apps" {
+  depends_on = [null_resource.copy_kubeconfig]
+
   provisioner "local-exec" {
     command = <<EOT
+      kubectl create namespace argocd &&
       kubectl apply -k https://github.com/argoproj/argo-cd/manifests/crds?ref=stable &&
       kubectl apply -f ${path.module}/app-of-apps.yaml
     EOT
